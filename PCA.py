8b409c39844b042d7541f7b6af058a10cec6395c
WANG Wei <m@example.com>
2019-04-30 13:19:46 +0800

    dimension reduction by PCA

--- PCA.py
+++ PCA.py
@@ -2,8 +2,9 @@ import pandas as pd
 import numpy as np
 from sklearn.decomposition import PCA
 
-
-df = pd.read_excel('jester-data-3.xls',header=None)
+ratings = pd.read_excel('jester-data-3.xls',header=None)
 # Data from 24,938 users who have rated between 15 and 35 jokes, a matrix with dimensions 24,938 X 101.
-
-print(df)
+pca = PCA(n_components=35)
+pca.fit(ratings)
+newRatings = pca.transform(ratings)
+print(newRatings)
